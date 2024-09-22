e872592d7c94aa4653df250cead81a9950bea4ad
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-09-22 04:44:36 +0300

    Add project files.

---
+++ SifreliVeriler/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace SifreliVeriler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.btnkaydet = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtsoyad = new System.Windows.Forms.TextBox();
+            this.txtmail = new System.Windows.Forms.TextBox();
+            this.txtsifre = new System.Windows.Forms.TextBox();
+            this.txthesapno = new System.Windows.Forms.TextBox();
+            this.btnsifrecoz = new System.Windows.Forms.Button();
+            this.lblcozum = new System.Windows.Forms.Label();
+            this.txtcozum = new System.Windows.Forms.TextBox();
+            this.txtcozulmusveri = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad :";
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(111, 5);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(220, 35);
+            this.txtad.TabIndex = 1;
+            // 
+            // btnkaydet
+            // 
+            this.btnkaydet.Location = new System.Drawing.Point(631, 108);
+            this.btnkaydet.Name = "btnkaydet";
+            this.btnkaydet.Size = new System.Drawing.Size(141, 39);
+            this.btnkaydet.TabIndex = 2;
+            this.btnkaydet.Text = "Kaydet";
+            this.btnkaydet.UseVisualStyleBackColor = true;
+            this.btnkaydet.Click += new System.EventHandler(this.btnkaydet_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 247);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(822, 300);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Soyad :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mail :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(471, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Şifre :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(413, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Hesap No :";
+            // 
+            // txtsoyad
+            // 
+            this.txtsoyad.Location = new System.Drawing.Point(111, 59);
+            this.txtsoyad.Name = "txtsoyad";
+            this.txtsoyad.Size = new System.Drawing.Size(220, 35);
+            this.txtsoyad.TabIndex = 8;
+            // 
+            // txtmail
+            // 
+            this.txtmail.Location = new System.Drawing.Point(111, 110);
+            this.txtmail.Name = "txtmail";
+            this.txtmail.Size = new System.Drawing.Size(220, 35);
+            this.txtmail.TabIndex = 9;
+            // 
+            // txtsifre
+            // 
+            this.txtsifre.Location = new System.Drawing.Point(552, 8);
+            this.txtsifre.Name = "txtsifre";
+            this.txtsifre.Size = new System.Drawing.Size(220, 35);
+            this.txtsifre.TabIndex = 10;
+            // 
+            // txthesapno
+            // 
+            this.txthesapno.Location = new System.Drawing.Point(552, 68);
+            this.txthesapno.Name = "txthesapno";
+            this.txthesapno.Size = new System.Drawing.Size(220, 35);
+            this.txthesapno.TabIndex = 11;
+            // 
+            // btnsifrecoz
+            // 
+            this.btnsifrecoz.Location = new System.Drawing.Point(729, 202);
+            this.btnsifrecoz.Name = "btnsifrecoz";
+            this.btnsifrecoz.Size = new System.Drawing.Size(84, 39);
+            this.btnsifrecoz.TabIndex = 13;
+            this.btnsifrecoz.Text = "Çöz";
+            this.btnsifrecoz.UseVisualStyleBackColor = true;
+            this.btnsifrecoz.Click += new System.EventHandler(this.btnsifrecoz_Click);
+            // 
+            // lblcozum
+            // 
+            this.lblcozum.AutoSize = true;
+            this.lblcozum.Location = new System.Drawing.Point(-5, 174);
+            this.lblcozum.Name = "lblcozum";
+            this.lblcozum.Size = new System.Drawing.Size(136, 29);
+            this.lblcozum.TabIndex = 14;
+            this.lblcozum.Text = "Şifreli Veri :";
+            // 
+            // txtcozum
+            // 
+            this.txtcozum.Location = new System.Drawing.Point(0, 206);
+            this.txtcozum.Name = "txtcozum";
+            this.txtcozum.Size = new System.Drawing.Size(331, 35);
+            this.txtcozum.TabIndex = 15;
+            // 
+            // txtcozulmusveri
+            // 
+            this.txtcozulmusveri.Location = new System.Drawing.Point(348, 206);
+            this.txtcozulmusveri.Name = "txtcozulmusveri";
+            this.txtcozulmusveri.Size = new System.Drawing.Size(375, 35);
+            this.txtcozulmusveri.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(343, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(180, 29);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Çözülmüş Veri :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 547);
+            this.Controls.Add(this.txtcozulmusveri);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtcozum);
+            this.Controls.Add(this.lblcozum);
+            this.Controls.Add(this.btnsifrecoz);
+            this.Controls.Add(this.txthesapno);
+            this.Controls.Add(this.txtsifre);
+            this.Controls.Add(this.txtmail);
+            this.Controls.Add(this.txtsoyad);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnkaydet);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.Button btnkaydet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtsoyad;
+        private System.Windows.Forms.TextBox txtmail;
+        private System.Windows.Forms.TextBox txtsifre;
+        private System.Windows.Forms.TextBox txthesapno;
+        private System.Windows.Forms.Button btnsifrecoz;
+        private System.Windows.Forms.Label lblcozum;
+        private System.Windows.Forms.TextBox txtcozum;
+        private System.Windows.Forms.TextBox txtcozulmusveri;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ SifreliVeriler/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SifreliVeriler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-32Q9FH5;Initial Catalog=Test;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
+
+        void listele()
+        {
+            baglanti.Open();
+            DataTable dt = new DataTable();
+
+            // Sütunları tanımla
+            dt.Columns.Add("Ad");
+            dt.Columns.Add("Soyad");
+            dt.Columns.Add("Mail");
+            dt.Columns.Add("Sifre");
+            dt.Columns.Add("HesapNo");
+
+            SqlCommand komut = new SqlCommand("Select AD, SOYAD, MAIL, SIFRE, HESAPNO From TBLVERILER", baglanti);
+            SqlDataReader dr = komut.ExecuteReader();
+
+            while (dr.Read())
+            {
+                DataRow row = dt.NewRow();
+
+                string adcozum = dr[0].ToString(); // AD sütunu 0. indeks
+                byte[] adcozumdizi = Convert.FromBase64String(adcozum);
+                row["Ad"] = Encoding.UTF8.GetString(adcozumdizi); // UTF-8 kullanıldı
+
+                string soyadcozum = dr[1].ToString(); // SOYAD sütunu 1. indeks
+                byte[] soyadcozumdizi = Convert.FromBase64String(soyadcozum);
+                row["Soyad"] = Encoding.UTF8.GetString(soyadcozumdizi); // UTF-8 kullanıldı
+
+                string mailcozum = dr[2].ToString(); // MAIL sütunu 2. indeks
+                byte[] mailcozumdizi = Convert.FromBase64String(mailcozum);
+                row["Mail"] = Encoding.UTF8.GetString(mailcozumdizi); // UTF-8 kullanıldı
+
+                string sifrecozum = dr[3].ToString(); // SIFRE sütunu 3. indeks
+                byte[] sifrecozumdizi = Convert.FromBase64String(sifrecozum);
+                row["Sifre"] = Encoding.UTF8.GetString(sifrecozumdizi); // UTF-8 kullanıldı
+
+                string hesapnocozum = dr[4].ToString(); // HESAPNO sütunu 4. indeks
+                byte[] hesapnocozumdizi = Convert.FromBase64String(hesapnocozum);
+                row["HesapNo"] = Encoding.UTF8.GetString(hesapnocozumdizi); // UTF-8 kullanıldı
+
+                dt.Rows.Add(row);
+            }
+
+
+            dataGridView1.DataSource = dt; // DataGridView'e veri kaynağı atanıyor
+            baglanti.Close();
+        }
+
+
+        private void btnkaydet_Click(object sender, EventArgs e)
+        {
+
+            //Encoding.UTF8 kullanırsanız türkçe karakterler gözükür
+            string ad = txtad.Text;
+            byte[] addizi = Encoding.UTF8.GetBytes(ad);
+            string adsifre = Convert.ToBase64String(addizi);
+
+            string soyad = txtsoyad.Text;
+            byte[] soyaddizi = Encoding.UTF8.GetBytes(soyad);
+            string soyadsifre = Convert.ToBase64String(soyaddizi);
+
+            string mail = txtmail.Text;
+            byte[] maildizi = Encoding.UTF8.GetBytes(mail);
+            string mailsifre = Convert.ToBase64String(maildizi);
+
+            string sifre = txtsifre.Text;
+            byte[] sifredizi = Encoding.UTF8.GetBytes(sifre);
+            string sifresifre = Convert.ToBase64String(sifredizi);
+
+            string hesapno = txthesapno.Text;
+            byte[] hesapnodizi = Encoding.UTF8.GetBytes(hesapno);
+            string hesapnosifre = Convert.ToBase64String(hesapnodizi);
+
+
+            // ASCIIEncoding.ASCII kullanırsanız veri okudugunda  türkçe harfler ? işareti cıkar
+            //string ad = txtad.Text;
+            //byte[] addizi = ASCIIEncoding.ASCII.GetBytes(ad);
+            //string adsifre = Convert.ToBase64String(addizi);
+
+            //string soyad = txtsoyad.Text;
+            //byte[] soyaddizi = ASCIIEncoding.ASCII.GetBytes(soyad);
+            //string soyadsifre = Convert.ToBase64String(soyaddizi);
+
+            //string mail = txtmail.Text;
+            //byte[] maildizi = ASCIIEncoding.ASCII.GetBytes(mail);
+            //string mailsifre = Convert.ToBase64String(maildizi);
+
+            //string sifre = txtsifre.Text;
+            //byte[] sifredizi = ASCIIEncoding.ASCII.GetBytes(sifre);
+            //string sifresifre = Convert.ToBase64String(sifredizi);
+
+            //string hesapno = txthesapno.Text;
+            //byte[] hesapnodizi = ASCIIEncoding.ASCII.GetBytes(hesapno);
+            //string hesapnosifre = Convert.ToBase64String(hesapnodizi);
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into TBLVERILER (AD,SOYAD,MAIL,SIFRE,HESAPNO) values (@p1,@p2,@p3,@p4,@p5)", baglanti);
+            komut.Parameters.AddWithValue("@p1", adsifre);
+            komut.Parameters.AddWithValue("@p2", soyadsifre);
+            komut.Parameters.AddWithValue("@p3", mailsifre);
+            komut.Parameters.AddWithValue("@p4", sifresifre);
+            komut.Parameters.AddWithValue("@p5", hesapnosifre);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Veriler Eklendi", "Bilgi");
+
+            listele();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void btnsifrecoz_Click(object sender, EventArgs e)
+        {
+            //string adcozum = txtad.Text;
+            //byte[] adcozumdizi = Convert.FromBase64String(adcozum);
+            //string adverisi = ASCIIEncoding.ASCII.GetString(adcozumdizi);
+
+            //string soyadcozum = txtsoyad.Text;
+            //byte[] soyadcozumdizi = Convert.FromBase64String(soyadcozum);
+            //string soyadverisi = ASCIIEncoding.ASCII.GetString(soyadcozumdizi);
+
+            //string mailcozum = txtmail.Text;
+            //byte[] mailcozumdizi = Convert.FromBase64String(mailcozum);
+            //string mailverisi = ASCIIEncoding.ASCII.GetString(mailcozumdizi);
+
+            //string sifrecozum = txtsifre.Text;
+            //byte[] sifrecozumdizi = Convert.FromBase64String(sifrecozum);
+            //string sifreverisi = ASCIIEncoding.ASCII.GetString(sifrecozumdizi);
+
+            //string hesapnocozum = txtmail.Text;
+            //byte[] hesapnocozumdizi = Convert.FromBase64String(hesapnocozum);
+            //string hesapnoverisi = ASCIIEncoding.ASCII.GetString(hesapnocozumdizi);
+
+
+            //// Ad çözümleme
+            //string adcozum = txtad.Text;
+            //byte[] adcozumdizi = Convert.FromBase64String(adcozum);
+            //string adverisi = Encoding.UTF8.GetString(adcozumdizi);
+
+            //// Soyad çözümleme
+            //string soyadcozum = txtsoyad.Text;
+            //byte[] soyadcozumdizi = Convert.FromBase64String(soyadcozum);
+            //string soyadverisi = Encoding.UTF8.GetString(soyadcozumdizi);
+
+            //// Mail çözümleme
+            //string mailcozum = txtmail.Text;
+            //byte[] mailcozumdizi = Convert.FromBase64String(mailcozum);
+            //string mailverisi = Encoding.UTF8.GetString(mailcozumdizi);
+
+            //// Şifre çözümleme
+            //string sifrecozum = txtsifre.Text;
+            //byte[] sifrecozumdizi = Convert.FromBase64String(sifrecozum);
+            //string sifreverisi = Encoding.UTF8.GetString(sifrecozumdizi);
+
+            //// Hesap No çözümleme
+            //string hesapnocozum = txthesapno.Text; // Doğru kontrol edin
+            //byte[] hesapnocozumdizi = Convert.FromBase64String(hesapnocozum);
+            //string hesapnoverisi = Encoding.UTF8.GetString(hesapnocozumdizi);
+
+            //çözmek istediğin
+            string cozum = txtcozum.Text;
+            byte[] cozumdizi = Convert.FromBase64String(cozum);
+            string cozumverisi = Encoding.UTF8.GetString(cozumdizi);
+            txtcozulmusveri.Text = cozumverisi;
+
+        }
+    }
+}
